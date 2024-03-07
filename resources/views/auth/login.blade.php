8986312d477bf6cb9ea39a19d4869c37ba6b8eef
maicolkutchoukov <maicol.j.95@gmail.com>
2024-03-07 19:19:25 +0100

    Migliorata grafica

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,43 +12,51 @@
             </ul>
         </div>
     @endif
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+    <div class="row justify-content-center align-items-center ">
+        <div class="col-6 border rounded py-5 px-5 text-white login-form">
+            <form method="POST" action="{{ route('login') }}" class="">
+                @csrf
+        
+                <!-- Email Address -->
+                <div>
+                    <label for="email" class="form-label">
+                        Email
+                    </label>
+                    <input class="form-control" type="email" id="email" name="email">
+                    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
 
-        <!-- Email Address -->
-        <div>
-            <label for="email">
-                Email
-            </label>
-            <input type="email" id="email" name="email">
+                </div>
+        
+                <!-- Password -->
+                <div class="mt-4">
+                    <label for="password" class="form-label">
+                        Password
+                    </label>
+                    <input class="form-control" type="password" id="password" name="password">
+                </div>
+        
+                <!-- Remember Me -->
+                <div class="mt-4">
+                    <label for="remember_me" class="form-label">
+                        <input class="form-check-input me-1" id="remember_me" type="checkbox" name="remember">
+                        <span class="form-check-label">Remember me</span>
+                    </label>
+                </div>
+        
+                <div class="mt-4">
+                    @if (Route::has('password.request'))
+
+                        <a href="{{ route('password.request') }}" class="forgot-link">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                        
+                    @endif
+        
+                    <button type="submit" class="btn btn-primary ms-3">
+                        Log in
+                    </button>
+                </div>
+            </form>
         </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <label for="password">
-                Password
-            </label>
-            <input type="password" id="password" name="password">
-        </div>
-
-        <!-- Remember Me -->
-        <div class="mt-4">
-            <label for="remember_me">
-                <input id="remember_me" type="checkbox" name="remember">
-                <span>Remember me</span>
-            </label>
-        </div>
-
-        <div class="mt-4">
-            @if (Route::has('password.request'))
-                <a href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <button type="submit">
-                Log in
-            </button>
-        </div>
-    </form>
+    </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,8 +11,8 @@
         @vite('resources/js/app.js')
     </head>
     <body>
-        <header>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <header class="">
+            <nav class="navbar navbar-expand-lg bg-body-secondary">
                 <div class="container">
                     <a class="navbar-brand" href="/">Template</a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
